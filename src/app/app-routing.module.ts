e1706f97fe6391ad703961aaa3d03c9e56f87acb
cristiandev <cristiansotomayor.dev@gmail.com>
2021-11-01 22:27:50 -0500

    app routing file

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+const routes: Routes = [
+  {
+    path: 'home', component: HomePageComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
