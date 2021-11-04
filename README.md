0ca50ed7b8bc909bfa8b1f9d074f917df8b7d76e
cristiandev <cristiansotomayor.dev@gmail.com>
2021-11-03 23:55:35 -0500

    adding jest config files and packages

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This repo has all the basic configurations for angular material to make fast pro
 - [x] Angular 12
 - [x] Angular Material
 - [x] Eslint
-- [ ] Jest
+- [x] Jest
 - [ ] Docker
 - [ ] Tailwind (maybe)
 
@@ -23,10 +23,6 @@ Run `ng generate component component-name` to generate a new component. You can 
 ## Build
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
 
 ## Running end-to-end tests
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,34 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './components/hello-world/hello-world.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HelloWorldComponent,
       ],
     }).compileComponents();
-  });
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'angular-material-boilerplate'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('angular-material-boilerplate');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-material-boilerplate app is running!');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent)
+      .toContain('This is the app component!');
   });
 });
